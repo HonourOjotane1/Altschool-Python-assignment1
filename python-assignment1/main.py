ec5ec1fc913725828be98c4a10a25d3d68a73498
Honour Ojotane <ojotanehonour@gmail.com>
2024-11-16 08:43:45 +0100

    checking things again

---
+++ python-assignment1/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel,EmailStr
+from typing import List
+import time
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+class User(BaseModel):
+    first_name:str
+    last_name:str
+    age:int
+    email:EmailStr
+    height:float
+
+user_data: List[User] = []
+
+def log_middleware(request:Request, call_next):
+    start_time = time.time()
+
+    response = call_next(request)
+    end_time = time.time()
+
+    duration = end_time - start_time
+    print(f"Request: {request.method} {request.url} | Duration: {duration:.4f} seconds")
+    return response
+
+app.middleware("http")(log_middleware)
+
+
+@app.post("Users", status_code=201)
+def create_user(user: User):
+    user_data(user)
+    return{
+        "message:" "user created successfulu!"
+    }
